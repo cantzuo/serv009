f94247bc6602835e7b7d6843035d800ee31c038b
cantzuo <156641993+cantzuo@users.noreply.github.com>
2024-12-28 15:38:29 +0800

    更新 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = '';
-let proxyIP = '91.185.190.159:51540';
+let proxyIP = '91.185.190.159:31301';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
